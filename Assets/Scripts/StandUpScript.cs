ef8798820b0f0048c2ac514f76fc749dba6b3c12
saltire sable <saltiresable@gmail.com>
2022-05-14 19:52:52 -0400

    Update standup script

--- Assets/Scripts/StandUpScript.cs
+++ Assets/Scripts/StandUpScript.cs
@@ -2,33 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StandUpScript : MonoBehaviour
-{
-    public float smoothTime = 0.3F;
-    private Vector3 velocity = Vector3.zero;
+public class StandUpScript : MonoBehaviour {
+    public float moveSpeed = 0.001f;
     public bool hasTriggered = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        this.transform.position = new Vector3(this.transform.position.x, 0.015f, this.transform.position.y);
+    Vector3 upPosition;
+    Vector3 downPosition;
+
+    void Start() {
+        upPosition = transform.position;
+        downPosition = transform.position + Vector3.down * transform.localScale.y;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (hasTriggered == true) {
-            this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(this.transform.localPosition.x, -0.0164f, 0.0107f), ref velocity, smoothTime);
+    void Update() {
+        if (hasTriggered && transform.position != downPosition) {
+            transform.position += Vector3.down
+                * Mathf.Min(moveSpeed, Vector3.Distance(transform.position, downPosition));
         }
-        else if (hasTriggered == false) {
-            this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(this.transform.position.x, 0.014f, 0.0107f), ref velocity, smoothTime);
+        else if (!hasTriggered && transform.position != downPosition){
+            transform.position += Vector3.up
+                * Mathf.Min(moveSpeed, Vector3.Distance(transform.position, upPosition));
         }
     }
 
-    public void OnTriggerEnter (Collider collider)
-    {
+    public void OnTriggerEnter (Collider collider) {
         if (collider.gameObject.tag == "Ball") {
             hasTriggered = true;
         }
-    }    
+    }
 }
